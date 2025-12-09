fab4d0c42d9615631597d0d5f933a3711a141608
dokin2542000-a <dokin2542000@gmail.com>
2025-12-10 00:18:24 +0700

    Cập nhật practice11

--- practice11/shoppe.js
+++ practice11/shoppe.js
@@ -1,25 +1,26 @@
 const data= [
-  { "product_image": "/images/img1.jpg", "name": "Vintage Typewriter", "price": 49.5, "created_at": "2025-07-20", "rating_point": 4.5, "location": "New York", "quantity": 3,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img2.jpg", "name": "Leatherbound Journal", "price": 25.0, "created_at": "2025-07-18", "rating_point": 4.7, "location": "San Francisco", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img3.jpg", "name": "Fountain Pen", "price": 30.0, "created_at": "2025-07-21", "rating_point": 4.2, "location": "Chicago", "quantity": 1,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img4.jpg", "name": "High-Contrast Keyboard", "price": 95.0, "created_at": "2025-07-19", "rating_point": 4.8, "location": "Seattle", "quantity": 4,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img5.jpg", "name": "Wireless Headphones", "price": 120.0, "created_at": "2025-07-17", "rating_point": 4.6, "location": "Los Angeles", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img6.jpg", "name": "Ceramic Coffee Mug", "price": 12.5, "created_at": "2025-07-15", "rating_point": 4.0, "location": "Austin", "quantity": 9,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img7.jpg", "name": "Desk Lamp", "price": 45.0, "created_at": "2025-07-22", "rating_point": 4.3, "location": "Boston", "quantity": 5,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img8.jpg", "name": "Classic Wristwatch", "price": 80.0, "created_at": "2025-07-20", "rating_point": 4.1, "location": "Denver", "quantity": 7,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img9.jpg", "name": "Potted Desk Plant", "price": 18.0, "created_at": "2025-07-21", "rating_point": 3.9, "location": "Portland", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img10.jpg", "name": "Film Camera", "price": 135.0, "created_at": "2025-07-16", "rating_point": 4.9, "location": "San Diego", "quantity": 8,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img11.jpg", "name": "Running Sneakers", "price": 60.0, "created_at": "2025-07-14", "rating_point": 4.2, "location": "Houston", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img12.jpg", "name": "Laptop Backpack", "price": 55.0, "created_at": "2025-07-13", "rating_point": 4.4, "location": "Atlanta", "quantity": 10,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img13.jpg", "name": "Modern Fiction Book", "price": 15.0, "created_at": "2025-07-19", "rating_point": 4.6, "location": "Phoenix", "quantity": 1,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img14.jpg", "name": "4K Monitor", "price": 220.0, "created_at": "2025-07-18", "rating_point": 4.8, "location": "Dallas", "quantity": 4,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img15.jpg", "name": "Wireless Mouse", "price": 25.0, "created_at": "2025-07-20", "rating_point": 4.1, "location": "Philadelphia", "quantity": 7,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img16.jpg", "name": "Mechanical Keyboard", "price": 110.0, "created_at": "2025-07-22", "rating_point": 4.7, "location": "Miami", "quantity": 3,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img17.jpg", "name": "Ergonomic Chair", "price": 180.0, "created_at": "2025-07-12", "rating_point": 4.5, "location": "Detroit", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img18.jpg", "name": "Leather Phone Case", "price": 20.0, "created_at": "2025-07-21", "rating_point": 4.0, "location": "Las Vegas", "quantity": 5,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img19.jpg", "name": "Digital Tablet", "price": 350.0, "created_at": "2025-07-22", "rating_point": 4.6, "location": "Orlando", "quantity": 9,"add_cart": "no","choose_quantity":0, "select":1},
-  { "product_image": "/images/img20.jpg", "name": "Insulated Water Bottle", "price": 22.0, "created_at": "2025-07-17", "rating_point": 4.3, "location": "Minneapolis", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1}
+  { "product_image": "product-img/img1.webp", "name": "Vintage Typewriter", "price": 49.5, "created_at": "2025-07-20", "rating_point": 4.5, "location": "New York", "quantity": 3,"add_cart": "no","choose_quantity":0, "select":1, "category": "Điện Thoại iPhone 16 Giá Rẻ"},
+  { "product_image": "product-img/img2.webp", "name": "Leatherbound Journal", "price": 25.0, "created_at": "2025-07-18", "rating_point": 4.7, "location": "San Francisco", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1, "category": "Điện Thoại iPhone 16 Giá Rẻ"},
+  { "product_image": "product-img/img3.webp", "name": "Fountain Pen", "price": 30.0, "created_at": "2025-07-21", "rating_point": 4.2, "location": "Chicago", "quantity": 1,"add_cart": "no","choose_quantity":0, "select":1, "category": "Điện Thoại iPhone 16 Giá Rẻ"},
+  { "product_image": "product-img/img4.webp", "name": "High-Contrast Keyboard", "price": 95.0, "created_at": "2025-07-19", "rating_point": 4.8, "location": "Seattle", "quantity": 4,"add_cart": "no","choose_quantity":0, "select":1, "category": "Quần dài"},
+  { "product_image": "product-img/img5.webp", "name": "Wireless Headphones", "price": 120.0, "created_at": "2025-07-17", "rating_point": 4.6, "location": "Los Angeles", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1, "category": "Quần dài"},
+  { "product_image": "product-img/img6.webp", "name": "Ceramic Coffee Mug", "price": 12.5, "created_at": "2025-07-15", "rating_point": 4.0, "location": "Austin", "quantity": 9,"add_cart": "no","choose_quantity":0, "select":1, "category": "Áo"},
+  { "product_image": "product-img/img7.webp", "name": "Desk Lamp", "price": 45.0, "created_at": "2025-07-22", "rating_point": 4.3, "location": "Boston", "quantity": 5,"add_cart": "no","choose_quantity":0, "select":1, "category": "Áo khoác gió"},
+  { "product_image": "product-img/img8.jpg", "name": "Classic Wristwatch", "price": 80.0, "created_at": "2025-07-20", "rating_point": 4.1, "location": "Denver", "quantity": 7,"add_cart": "no","choose_quantity":0, "select":1, "category": "Áo"},
+  { "product_image": "product-img/img9.jpg", "name": "Potted Desk Plant", "price": 18.0, "created_at": "2025-07-21", "rating_point": 3.9, "location": "Portland", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1, "category": "Áo"},
+  { "product_image": "product-img/img10.webp", "name": "Film Camera", "price": 135.0, "created_at": "2025-07-16", "rating_point": 4.9, "location": "San Diego", "quantity": 8,"add_cart": "no","choose_quantity":0, "select":1, "category": "Dép"},
+  { "product_image": "product-img/img11.webp", "name": "Running Sneakers", "price": 60.0, "created_at": "2025-07-14", "rating_point": 4.2, "location": "Houston", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1, "category": "Dép"},
+  { "product_image": "product-img/img12.webp", "name": "Laptop Backpack", "price": 55.0, "created_at": "2025-07-13", "rating_point": 4.4, "location": "Atlanta", "quantity": 10,"add_cart": "no","choose_quantity":0, "select":1, "category": "Dép"},
+  { "product_image": "product-img/img13.webp", "name": "Modern Fiction Book", "price": 15.0, "created_at": "2025-07-19", "rating_point": 4.6, "location": "Phoenix", "quantity": 1,"add_cart": "no","choose_quantity":0, "select":1, "category": "Tai Nghe giá Rẻ"},
+  { "product_image": "product-img/img14.webp", "name": "4K Monitor", "price": 220.0, "created_at": "2025-07-18", "rating_point": 4.8, "location": "Dallas", "quantity": 4,"add_cart": "no","choose_quantity":0, "select":1, "category": "Tai Nghe giá Rẻ"},
+  { "product_image": "product-img/img15.webp", "name": "Wireless Mouse", "price": 25.0, "created_at": "2025-07-20", "rating_point": 4.1, "location": "Philadelphia", "quantity": 7,"add_cart": "no","choose_quantity":0, "select":1, "category": "Kính Gương"},
+  { "product_image": "product-img/img16.webp", "name": "Mechanical Keyboard", "price": 110.0, "created_at": "2025-07-22", "rating_point": 4.7, "location": "Miami", "quantity": 3,"add_cart": "no","choose_quantity":0, "select":1, "category": "Kính Gương"},
+  { "product_image": "product-img/img17.webp", "name": "Ergonomic Chair", "price": 180.0, "created_at": "2025-07-12", "rating_point": 4.5, "location": "Detroit", "quantity": 2,"add_cart": "no","choose_quantity":0, "select":1, "category": "Kính Gương"},
+  { "product_image": "product-img/img18.webp", "name": "Leather Phone Case", "price": 20.0, "created_at": "2025-07-21", "rating_point": 4.0, "location": "Las Vegas", "quantity": 5,"add_cart": "no","choose_quantity":0, "select":1, "category": "Quần dài"},
+  { "product_image": "product-img/img19.webp", "name": "Digital Tablet", "price": 350.0, "created_at": "2025-07-22", "rating_point": 4.6, "location": "Orlando", "quantity": 9,"add_cart": "no","choose_quantity":0, "select":1, "category": "Tai Nghe giá Rẻ"},
+  { "product_image": "product-img/img20.webp", "name": "Insulated Water Bottle", "price": 22.0, "created_at": "2025-07-17", "rating_point": 4.3, "location": "Minneapolis", "quantity": 6,"add_cart": "no","choose_quantity":0, "select":1, "category": "Tai Nghe giá Rẻ"}
 ]
+// localStorage.setItem('products', JSON.stringify(data))
 let inf = JSON.parse(localStorage.getItem('products'));
 localStorage.removeItem('product');
 // Nếu chưa có dữ liệu (lần đầu mở), khởi tạo mặc định và lưu
@@ -45,6 +46,7 @@ var hideBasket=0
 //---hiển thị danh sách sản phẩm
 var numberSelect=0//sp+search
 addCart()
+count()
 loadingPage()
 function loadingPage(){
   numberSelect=0
@@ -65,7 +67,7 @@ function loadingPage(){
     clone.style.display='block'
     const index=m-a+x
     clone.querySelector('.list-product-databox #list-product-data-name').textContent= inf[index].name
-    clone.querySelector('.list-product-databox #list-product-data-price').textContent=inf[index].price
+    clone.querySelector('.list-product-databox #list-product-data-price').textContent=inf[index].price + "  SL: " +inf[index].quantity
     clone.querySelector('.list-product-databox #list-product-data-point').textContent=inf[index].rating_point
     clone.querySelector('.list-product-databox #list-product-data-location').textContent=inf[index].location
     clone.querySelector('.list-product-databox .list-product-data-pt img').src=inf[index].product_image
@@ -78,7 +80,21 @@ function loadingPage(){
         inf[index].choose_quantity=1
         localStorage.setItem('products', JSON.stringify(inf))
         addCart() 
+        count()
+        right()
       }  
+      else{
+        if(inf[index].choose_quantity<inf[index].quantity){
+          inf[index].choose_quantity+=1
+          localStorage.setItem('products', JSON.stringify(inf))
+          addCart()
+          count()
+          right()
+        }
+        else{
+          alert('Số lượng sản phẩm này đã hết!')
+        }
+      }
     }
     if(z==numberSelect) {break}
   }
@@ -152,16 +168,17 @@ function addCart(){
       // clone.style.display=''
       clone.querySelector('.product-img img').src=choose.product_image
       clone.querySelector('.product-nameprice .product-name').textContent=choose.name
-      clone.querySelector('.product-nameprice .product-price').textContent=choose.price + 'đ'
+      clone.querySelector('.product-nameprice .product-price').textContent='Hiện có: ' +choose.quantity +'  Giá: '+ choose.price + 'đ' + '   ' 
       clone.querySelector('.quantity').textContent=choose.choose_quantity
       list1.appendChild(clone)
       push=clone.querySelector('.plus')
       push.onclick= function(){        
-        if(inf[index].choose_quantity<=inf[index].quantity){
+        if(inf[index].choose_quantity<inf[index].quantity){
           inf[index].choose_quantity+=1
           clone.querySelector('.quantity').textContent=inf[index].choose_quantity
           localStorage.setItem('products', JSON.stringify(inf))
           addCart()
+          count()
         }
       }
       minus=clone.querySelector('.minus')
@@ -175,6 +192,7 @@ function addCart(){
             inf[index].add_cart='no'
             localStorage.setItem('products', JSON.stringify(inf))
             addCart()
+            count()
           }
         }
       }
@@ -184,6 +202,7 @@ function addCart(){
         inf[index].choose_quantity=0
         localStorage.setItem('products', JSON.stringify(inf))
         addCart()
+        count()
       }
         total=inf[index].choose_quantity*inf[index].price
         toTal+=total
@@ -194,7 +213,9 @@ function addCart(){
 
 //---checkout
 function checkOut(){
-  window.location.replace('shoppe-checkout.html')
+  if(document.getElementById('haveType').textContent!=0){
+    window.location.replace('shoppe-checkout.html')
+  }
 }
 
 const infor = document.getElementById('search')
@@ -208,7 +229,7 @@ function search(){
   localStorage.removeItem('product');
   const infor = document.getElementById('search')
   inf=JSON.parse(localStorage.getItem('products'))
-  inf=inf.filter(line => line.name.includes(infor.value.trim()))
+  inf=inf.filter(line => line.name.trim().toLowerCase().includes(infor.value.trim().toLowerCase())||line.category.trim().toLowerCase().includes(infor.value.trim().toLowerCase()))
   localStorage.setItem('product', JSON.stringify(inf));
   z=0 
   b=0
@@ -217,5 +238,36 @@ function search(){
   a=0
   x=0
   loadingPage()
-  document.querySelector('.search-notification p').textContent='Kết quả tìm kiếm cho từ khóa '+ "'" + infor.value.trim() + "'"
+  document.querySelector('.search-notification p').textContent='Kết quả tìm kiếm cho từ khóa '+ "'" + infor.value + "'"
+}
+
+//hiển thị số sp hiện có trong giỏ hàng
+function count(){
+  let count=0
+  inf=JSON.parse(localStorage.getItem('products'))
+  inf.forEach(infor => {
+    if(infor.add_cart=='yes')
+      count+=1
+  })
+  document.getElementById('haveType').textContent=count
+}
+
+//---lọc theo category
+const suggestItems = document.querySelectorAll('.suggest div')
+suggestItems.forEach(item=>{
+  item.onclick= function(){
+    document.getElementById('search').value=item.textContent
+    search()
+  }
+})
+
+//---thông báo cho vào giỏ hàng
+function right(){
+  document.querySelector('.success-box').style.display='block'
+  document.querySelector('.success-box').onclick= function(){
+    document.querySelector('.success-box').style.display='none'
+  }
+  setTimeout(function() {
+    document.querySelector('.success-box').style.display='none'
+}, 1500);
 }
--- practice11/shoppy-checkout.js
+++ practice11/shoppy-checkout.js
@@ -14,6 +14,22 @@ function order(){
         document.querySelector('#customer-name').textContent=document.getElementById('Fullname').value
         document.querySelector('#customer-city').textContent=document.getElementById('City').value
         document.querySelector('#customer-address').textContent=document.getElementById('Address').value
+        inf=JSON.parse(localStorage.getItem('products'))
+        for (let index = inf.length - 1; index >= 0; index--) {
+            if (inf[index].add_cart === 'yes') {
+                inf[index].quantity -= inf[index].choose_quantity;
+                inf[index].choose_quantity = 0;
+                inf[index].add_cart = 'no';
+
+        // Nếu hết hàng thì xoá luôn
+                if (inf[index].quantity === 0) {
+                    inf.splice(index, 1);
+                }
+            }
+        }
+
+        // Chỉ lưu 1 lần sau khi xử lý xong
+        localStorage.setItem('products', JSON.stringify(inf));
     }
 }
 function cancel(){
